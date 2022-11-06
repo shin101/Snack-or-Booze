5ea969ba23af7bfe745f53e460766c1c9f37675d
shin101 <shinj0127@gmail.com>
2022-11-06 14:40:41 -0800

    90% of code works

--- snack-or-booze/src/NewFoodForm.js
+++ snack-or-booze/src/NewFoodForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 function NewFoodForm({ addFood }) {
+
   const [formData, setFormData] = useState({});
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -8,11 +9,15 @@ function NewFoodForm({ addFood }) {
       ...data,
       [name] : value
     }))
+    console.log({name, value})
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addFood({...formData});
+    addFood({...formData, id: formData.name});
+    setFormData({});
+    // reset empties input field
+    e.target.reset();
   }
 
   // let food = items.find(food => food.id === id);
@@ -20,21 +25,40 @@ function NewFoodForm({ addFood }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>Choose food type</p>
       <div id="dropdown">
       <select 
       name="type"
       onChange={handleChange}>
-        <option value="snacks">Snacks</option>
-        <option value="drinks">Drinks</option>
+        <option value="snacks" >Snacks</option>
+        <option value="drinks" selected>Drinks</option>
       </select>
       </div>
 
-      <div id="input">
+      <div id="foodname">
       <input
       name="name"
       placeholder= "food name here"
       onChange= {handleChange} /></div>
+
+      <div id="description">
+      <input
+      name="description"
+      placeholder= "description"
+      onChange= {handleChange} /></div>
+
+      <div id="recipe">
+      <input
+      name="recipe"
+      placeholder= "recipe here"
+      onChange= {handleChange} /></div>
+
+      <div id="serve">
+      <input
+      name="serve"
+      placeholder= "serve info"
+      onChange= {handleChange} /></div>
+
+
       <button>Add food</button>
     </form>
   );
