cf8ee5c63b24b0dfb0121afc7390858016c108f8
shin101 <shinj0127@gmail.com>
2022-11-05 23:22:31 -0700

    finished part 2

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,4 +1,15 @@
 function unroll(squareArray) {
+  let arr = [];
+
+  while(squareArray.length) {
+    arr.push(
+      ...squareArray.shift(), //first row
+      ...squareArray.map(n => n.pop()), 
+      ...(squareArray.pop() || []).reverse(),
+      ...squareArray.map(n => n.shift()).reverse()
+    );
+  }
+  return arr;
 
 }
 
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,21 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("unrolls an array of numbers", function () {
+    expect(unroll([
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]
+    ])).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+  });
+
+  it("unrolls an array of letters", function () {
+    expect(unroll([
+      ["a", "b", "c"],
+      ["d", "e", "f"],
+      ["g", "h", "i"]
+    ])).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
+  });
+
 });
